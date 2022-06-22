b143c16b48d408919ba5558aed482c0f12275e13
TranVN <coppership1@gmail.com>
2022-06-22 15:46:51 +0700

    add new

---
+++ shop-tv/resources/views/layouts/menu.blade.php
@@ -0,0 +1,21 @@
+<div class="header-top"></div>
+<div class="header-main">
+    <div class="header-content d-flex">
+        <div class="logo align-items-start col-2">
+            <img src="{{asset('assets/logo.png')}}" alt="" srcset="">
+        </div>
+        <div class="search-form align-items-center col-8">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Tìm sản phẩm" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                  <button class="btn btn-secondary" type="button">
+                        <i class="fas fa-search"></i>
+                  </button>
+                </div>
+            </div>
+        </div>
+        <div class="card-button align-items-end col-2">
+            3
+        </div>
+    </div>
+</div>
---
+++ shop-tv/resources/views/shop/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.guest')
+@section('title')
+Trang chủ cửa hàng Thợ Việt
+@endsection
+@section('content')
+    @include('layouts.menu')
+    <h1>home page</h1>
+@endsection
